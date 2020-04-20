7ba224841013fc10e0a7be342794449ab3934c13
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-04-20 16:23:05 +0300

    Updates

---
+++ precache-manifest.07df987cf44f7b6ab27772d374243fd7.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52089ca80246cbbe79297fe56a6c6ef",
+    "url": "https://easably.github.io/website/index.html"
+  },
+  {
+    "revision": "b8214b4c456b768f8637",
+    "url": "https://easably.github.io/website/static/css/2.8c23d2df.chunk.css"
+  },
+  {
+    "revision": "ed60449ad8eb6f0532be",
+    "url": "https://easably.github.io/website/static/css/main.163bc7f5.chunk.css"
+  },
+  {
+    "revision": "b8214b4c456b768f8637",
+    "url": "https://easably.github.io/website/static/js/2.ae00604d.chunk.js"
+  },
+  {
+    "revision": "ed60449ad8eb6f0532be",
+    "url": "https://easably.github.io/website/static/js/main.6f0d5ab3.chunk.js"
+  },
+  {
+    "revision": "aadf42645a737281a5a1",
+    "url": "https://easably.github.io/website/static/js/runtime-main.80fcfedf.js"
+  },
+  {
+    "revision": "227e6d38bd326c96a47d8971f0c253c2",
+    "url": "https://easably.github.io/website/static/media/1.227e6d38.png"
+  },
+  {
+    "revision": "d005094c458e537d36f19524050d3d28",
+    "url": "https://easably.github.io/website/static/media/1.d005094c.png"
+  },
+  {
+    "revision": "b8607a0a2f4d4d20bcebb15a06fa9f3d",
+    "url": "https://easably.github.io/website/static/media/10.b8607a0a.png"
+  },
+  {
+    "revision": "bde6a092abf58323d989b7d74425681a",
+    "url": "https://easably.github.io/website/static/media/10.bde6a092.png"
+  },
+  {
+    "revision": "529b325c83bc2fb7004f1e41343631bf",
+    "url": "https://easably.github.io/website/static/media/12-1.529b325c.png"
+  },
+  {
+    "revision": "345b556226420b9fb70e8007d9aea960",
+    "url": "https://easably.github.io/website/static/media/2.345b5562.png"
+  },
+  {
+    "revision": "475fc709303c035969050e5ec609b7db",
+    "url": "https://easably.github.io/website/static/media/2.475fc709.png"
+  },
+  {
+    "revision": "251e3546073ef03e5ea80b493e35c1e0",
+    "url": "https://easably.github.io/website/static/media/3.251e3546.png"
+  },
+  {
+    "revision": "a7a71492b76a3cb3079b6bd08808ce83",
+    "url": "https://easably.github.io/website/static/media/3.a7a71492.png"
+  },
+  {
+    "revision": "79388c049390e5369eaa4ab65c822edd",
+    "url": "https://easably.github.io/website/static/media/4.79388c04.png"
+  },
+  {
+    "revision": "83e9556fcc97bda61e4922f4cfe1d810",
+    "url": "https://easably.github.io/website/static/media/4.83e9556f.png"
+  },
+  {
+    "revision": "781c8f2b7eac0d58cc9a3803958afdee",
+    "url": "https://easably.github.io/website/static/media/404.781c8f2b.svg"
+  },
+  {
+    "revision": "e6982d7e52b7cfa549a65c519cd68262",
+    "url": "https://easably.github.io/website/static/media/5.e6982d7e.png"
+  },
+  {
+    "revision": "ee20f6ea56f96f944ceb497fd4584a8b",
+    "url": "https://easably.github.io/website/static/media/5.ee20f6ea.png"
+  },
+  {
+    "revision": "37dc94bddfa6d08cd385881a3d58b42d",
+    "url": "https://easably.github.io/website/static/media/6.37dc94bd.png"
+  },
+  {
+    "revision": "ee20f6ea56f96f944ceb497fd4584a8b",
+    "url": "https://easably.github.io/website/static/media/6.ee20f6ea.png"
+  },
+  {
+    "revision": "032815edab1903dcb0f07f67c1fe9230",
+    "url": "https://easably.github.io/website/static/media/7.032815ed.png"
+  },
+  {
+    "revision": "3b52395d105d09211ec4f5494eef23a4",
+    "url": "https://easably.github.io/website/static/media/7.3b52395d.png"
+  },
+  {
+    "revision": "73084e50f802b95d19de116fd049ce14",
+    "url": "https://easably.github.io/website/static/media/8.73084e50.png"
+  },
+  {
+    "revision": "7a402256337b1443a9e659977266c2e5",
+    "url": "https://easably.github.io/website/static/media/8.7a402256.png"
+  },
+  {
+    "revision": "c75bd7366bc860b81e5cd3224079569a",
+    "url": "https://easably.github.io/website/static/media/9.c75bd736.png"
+  },
+  {
+    "revision": "f5107e80f34437061223bd1b41b5ffbb",
+    "url": "https://easably.github.io/website/static/media/9.f5107e80.png"
+  },
+  {
+    "revision": "1ff484a8549b24a1f7a5219840b77797",
+    "url": "https://easably.github.io/website/static/media/BlogTop.1ff484a8.jpg"
+  },
+  {
+    "revision": "f3f9c6f316943b53c76bc37b169fde18",
+    "url": "https://easably.github.io/website/static/media/ExtensionLogo.f3f9c6f3.png"
+  },
+  {
+    "revision": "941390b1c6edeaf5c8bbff75e1be0048",
+    "url": "https://easably.github.io/website/static/media/Illustration6(3).941390b1.svg"
+  },
+  {
+    "revision": "517e2341786d1edd5e4b0b8fc7625741",
+    "url": "https://easably.github.io/website/static/media/PlayerLogo.517e2341.png"
+  },
+  {
+    "revision": "7e6d2b57dd5f5b98ac39f1d33fa4d8e1",
+    "url": "https://easably.github.io/website/static/media/Triangle1.1.7e6d2b57.svg"
+  },
+  {
+    "revision": "a3edc8b5fe232462667de246e23ad40c",
+    "url": "https://easably.github.io/website/static/media/Triangle1.a3edc8b5.svg"
+  },
+  {
+    "revision": "1fc63ca27bb450582a6b99d47838ea04",
+    "url": "https://easably.github.io/website/static/media/bcg3.1fc63ca2.svg"
+  },
+  {
+    "revision": "9a305b5326124b9ba4fecf17fb7f73e0",
+    "url": "https://easably.github.io/website/static/media/bcgPlans.9a305b53.svg"
+  },
+  {
+    "revision": "1ec74df600805d5f2f987bf2f84b9edd",
+    "url": "https://easably.github.io/website/static/media/bcgPlansMobile.1ec74df6.jpg"
+  },
+  {
+    "revision": "6f62d14b7214544695a2110f25f2a8b0",
+    "url": "https://easably.github.io/website/static/media/bcgPlayerTop.6f62d14b.png"
+  },
+  {
+    "revision": "a99263883650d20fcf96f7c5213b5544",
+    "url": "https://easably.github.io/website/static/media/bcgTop.a9926388.png"
+  },
+  {
+    "revision": "b52477cd3c2721e788052691986f88b8",
+    "url": "https://easably.github.io/website/static/media/bottom_flowers.b52477cd.png"
+  },
+  {
+    "revision": "83bc0300d92a3fdd68fcc58c24d70a7e",
+    "url": "https://easably.github.io/website/static/media/chromeStore.83bc0300.svg"
+  },
+  {
+    "revision": "4e8d5d0b68fbdd13b076d1746a22030b",
+    "url": "https://easably.github.io/website/static/media/devices_image.4e8d5d0b.png"
+  },
+  {
+    "revision": "0416a95e773cc5df31c9c14aa9a412c8",
+    "url": "https://easably.github.io/website/static/media/imgEnjoy.0416a95e.svg"
+  },
+  {
+    "revision": "f1bcbe70982c51d6c4a734d3bee4f2d5",
+    "url": "https://easably.github.io/website/static/media/phone.f1bcbe70.svg"
+  },
+  {
+    "revision": "05b20961038151089548c47302b34d80",
+    "url": "https://easably.github.io/website/static/media/phoneBackground.05b20961.svg"
+  },
+  {
+    "revision": "5d357ce81ef81291599aec637f859baa",
+    "url": "https://easably.github.io/website/static/media/productsFirstNumber.5d357ce8.svg"
+  },
+  {
+    "revision": "73c5243bfce9f683a1a759ee2dc3c547",
+    "url": "https://easably.github.io/website/static/media/productsSecondNumber.73c5243b.svg"
+  },
+  {
+    "revision": "9f647bb4252b087ca72482473b4e5693",
+    "url": "https://easably.github.io/website/static/media/puzzle.9f647bb4.svg"
+  },
+  {
+    "revision": "0f4abafb24d8a4c8764dd064248fb69a",
+    "url": "https://easably.github.io/website/static/media/repeat.0f4abafb.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://easably.github.io/website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://easably.github.io/website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://easably.github.io/website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://easably.github.io/website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "88d5df221c74269affe92bfefbf42c5f",
+    "url": "https://easably.github.io/website/static/media/subtitles.88d5df22.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://easably.github.io/website/precache-manifest.0e2e0ddbe5f91afe6d47dba62066c1f9.js"
+  "https://easably.github.io/website/precache-manifest.07df987cf44f7b6ab27772d374243fd7.js"
 );
 
 self.addEventListener('message', (event) => {
