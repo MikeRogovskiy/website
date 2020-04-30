0dd7690dbb0bcbb1085520a7986dba8d30f3929d
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-04-30 13:28:03 +0300

    Accourding to Issue #32 new design without correct functionality and animation is added

--- src/pages/Plans/index.jsx
+++ src/pages/Plans/index.jsx
@@ -4,9 +4,7 @@ import MainHeader from "../../components/MainHeader";
 
 import "./Plans.scss";
 
-import blue_dot from '../../assets/images/Plans/blue_dot.png';
-import green_dot from '../../assets/images/Plans/green_dot.png';
-import yellow_dot from '../../assets/images/Plans/yellow_dot.png';
+import plansV from "../../assets/images/Plans/plansV.svg";
 
 export default class Plans extends Component {
 
@@ -15,19 +13,6 @@ export default class Plans extends Component {
   }
 
   render() {
-
-    const dollarSize = { fontSize: '1.8rem' }
-
-    const MonthStyles = {
-      color: 'silver',
-      fontSize: "1.8rem",
-    }
-
-    const premiumColor = { color: '#F2994A'}
-    const premiumBackgroundColor = { backgroundColor: '#F2994A' };
-    const premiumBorder = { border: '3.5px solid #34D086' }
-    const superPremiumColor = { color: '#34D086'}
-    const superPremiumBackgroundColor = { backgroundColor: '#34D086' };
 
     return (
       <div>
@@ -39,95 +24,85 @@ export default class Plans extends Component {
               <h1 className="head-title">{this.getLangText("PlansTitle")}</h1>
               <p className="head-subtitle">{this.getLangText("1-PlansSubtitle")}</p>
               <p className="head-subtitle">{this.getLangText("2-PlansSubtitle")}</p>
-              <p className="head-subtitle">{this.getLangText("3-PlansSubtitle")}</p>
             </div>
 
             <div className="plans_wrapper_container">
-              <container className="container-item">
+              <div className="container-item">
                 <div className="container-item_content">
                   <div className="container-item_content_header">
-                    <h2>{this.getLangText("Standart")}</h2>
-                    <h1>{this.getLangText("Free")}</h1>
+                    <h2 id="unique-item-paragraph">{this.getLangText("Standard")}</h2>
+                    <h1>{this.getLangText("StandardCondition")}</h1>
+                    <h3>{this.getLangText("StandardSubtitle")}</h3>
                   </div>
                   <div className="container-item_content_main">
                     <div className="main-item">
-                      <img src={blue_dot} />
-                      <p>{this.getLangText("1-StandartPoint")}</p>
+                      <img src={plansV} alt="v-image"/>
+                      <p>{this.getLangText("1-StandardPoint")}</p>
                     </div>
                     <div className="main-item">
-                      <img src={blue_dot} />
-                      <p>{this.getLangText("2-StandartPoint")}</p>
+                      <p>{this.getLangText("2-StandardPoint")}</p>
                     </div>
+                  </div>
+                  <div className="container-item_content_footer">
+                    <input type="button" className="container-item_content_footer_btn"
+                           target="_blank"  rel="noopener noreferrer" id="unique-item-button"
+                           onClick={myFunction} value={this.getLangText("ButtonName")}>
+                    </input>
                   </div>
                 </div>
-              </container>
+              </div>
 
-              <container className="container-item">
+              <div className="container-item">
                 <div className="container-item_content">
                   <div className="container-item_content_header">
-                    <h2 style={premiumColor}>{this.getLangText("Premium")}</h2>
-                    <h1><span style={dollarSize}>$</span>
-                      7.99
-                      <span style={MonthStyles}>{this.getLangText("Month")}</span>
-                    </h1>
+                    <h2>{this.getLangText("Premium")}</h2>
+                    <h1>{this.getLangText("PremiumCondition")}</h1>
+                    <h3>{this.getLangText("PremiumSubtitle")}</h3>
                   </div>
                   <div className="container-item_content_main">
                     <div className="main-item">
-                      <img src={yellow_dot} />
+                      <img src={plansV} alt="v-image"/>
                       <p>{this.getLangText("1-PremiumPoint")}</p>
                     </div>
                     <div className="main-item">
-                      <img src={yellow_dot} />
+                      <img src={plansV} alt="v-image"/>
                       <p>{this.getLangText("2-PremiumPoint")}</p>
                     </div>
                   </div>
                   <div className="container-item_content_footer">
-                    <a
-                        className="container-item_content_footer_btn top"
-                        style={premiumBackgroundColor}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        onClick={myFunction}
-                    >
-                      <span>{this.getLangText("Choose")}</span>
-                    </a>
+                    <input type="button" className="container-item_content_footer_btn"
+                           target="_blank"  rel="noopener noreferrer"
+                           onClick={myFunction} value={this.getLangText("ButtonName")}>
+                    </input>
                   </div>
                 </div>
-              </container>
+              </div>
 
-              <container className="container-item" style={premiumBorder}>
+              <div className="container-item">
                 <div className="container-item_content">
                   <div className="container-item_content_header">
-                    <h2 style={superPremiumColor}>{this.getLangText("SuperPremium")}</h2>
-                    <h1><span style={dollarSize}>$</span>
-                      4.99
-                      <span style={MonthStyles}>{this.getLangText("Month")}</span>
-                    </h1>
-                    <p>{this.getLangText("PremiumSubtitle")}</p>
+                    <h2>{this.getLangText("SuperPremium")}</h2>
+                    <h1>{this.getLangText("SuperPremiumCondition")}</h1>
+                    <h3>{this.getLangText("SuperPremiumSubtitle")}</h3>
                   </div>
                   <div className="container-item_content_main">
                     <div className="main-item">
-                      <img src={green_dot} />
+                      <img src={plansV} alt="v-image"/>
                       <p>{this.getLangText("1-SuperPremiumPoint")}</p>
                     </div>
                     <div className="main-item">
-                      <img src={green_dot} />
+                      <img src={plansV} alt="v-image"/>
                       <p>{this.getLangText("2-SuperPremiumPoint")}</p>
                     </div>
                   </div>
                   <div className="container-item_content_footer">
-                    <a
-                        className="container-item_content_footer_btn top"
-                        style={superPremiumBackgroundColor}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        onClick={myFunction}
-                    >
-                      <span>{this.getLangText("Choose")}</span>
-                    </a>
+                    <input type="button" className="container-item_content_footer_btn"
+                           target="_blank"  rel="noopener noreferrer"
+                           onClick={myFunction} value={this.getLangText("ButtonName")}>
+                    </input>
                   </div>
                 </div>
-              </container>
+              </div>
             </div>
           </div>
         </div>
