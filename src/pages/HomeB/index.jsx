996e8b74b7a548c0882379ba1df4b8f5b122ac18
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-05-22 14:27:16 +0300

    Some changes at /extension

--- src/pages/HomeB/index.jsx
+++ src/pages/HomeB/index.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import "./homeB.scss";
-// import testBPage from "../../assets/images/test/testMainPage.svg"
+import step1 from "../../assets/images/test/step1Test.svg"
 
 export default class HomeB extends Component {
     render(){
         return (
             <div id="homeB-container">
                 <div id="homeB-container_page">
-                    {/* <div className="homeB-container_page_buttons-folder">
-                        <button id="start-button"></button>
-                    </div> */}
+                    <div className="homeB-container_page_buttons-folder-A">
+                        <button className="start-button">НАЧАТЬ БЕСПЛАТНО</button>
+                    </div>
+                    <div className="homeB-container_page_content">
+                        <img src={step1}></img>
+                    </div>
+                    <div className="homeB-container_page_buttons-folder-B">
+                        <button className="start-button">НАЧАТЬ БЕСПЛАТНО</button>
+                    </div>
                 </div>
             </div>
         )
