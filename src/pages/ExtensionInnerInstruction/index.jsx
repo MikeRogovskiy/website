531e8b74c562941d1468d30cf1dd775870b97340
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-05-26 14:26:36 +0300

    Added correct size of the image

--- src/pages/ExtensionInnerInstruction/index.jsx
+++ src/pages/ExtensionInnerInstruction/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+import "./extensionInnerInstruction.scss";
+
 import innerInstruction from "../../assets/images/extension-inner-instruction/mainInnerInstruction.svg";
 
 export default class ExtensionInnerInstruction extends Component {
     render(){
         return(
-            <div>
-                <img src={innerInstruction} width="314px" height="354px"></img>
+            <div id="inner-instruction-container">
+                <img src={innerInstruction}></img>
             </div>
         )
     }
