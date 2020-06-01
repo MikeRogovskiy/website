23c66a9a34b23bd063bc7fc8b8d6798b3e5d0388
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-06-01 16:32:52 +0300

    Added check of OS and appearing of correct button accourding to current OS of client.

--- src/pages/Player/Footer.jsx
+++ src/pages/Player/Footer.jsx
@@ -13,26 +13,47 @@ const Footer = (props) => {
     const macLink = 'http://easylang.app/downloads/EasyLang.Player-Beta_mac_prod.dmg';
     const windowsLink = 'http://easylang.app/downloads/EasyLang.Player-Beta_win_prod.exe';
 
+    let OSName="Unknown OS";
+
+    function findOSName(){
+       
+        if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+        if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+        // if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+        // if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+    }
+    findOSName()
+
     return (
         <footer className="getStarted-footer">
 
             <div className="getStartedPlayer-button">
-                <a
-                    className="startedPlayer-button top"
-                    href={macLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <span>{getLangText("GetPlayerButtonMac")}</span>
-                </a>
-                <a
-                    className="startedPlayer-button top"
-                    href={windowsLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <span>{getLangText("GetPlayerButtonWindows")}</span>
-                </a>
+                    { OSName === "Windows" &&
+                        <a
+                            className="startedPlayer-button top"
+                            href={windowsLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <span>{getLangText("GetPlayerButtonWindows")}</span>
+                        </a>
+                    }
+                    { OSName === "Windows" &&
+                        <p>{getLangText("IfButtonIsForWindows")} <a href={macLink}>{getLangText("LinkToAnotherOSDownload")}</a></p>
+                    }
+                    { OSName === "MacOS" &&
+                        <a
+                            className="startedPlayer-button top"
+                            href={windowsLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <span>{getLangText("GetPlayerButtonMac")}</span>
+                        </a>
+                    }
+                    { OSName === "MacOS" &&
+                        <p>{getLangText("IfButtonIsForMac")} <a href={windowsLink}>{getLangText("LinkToAnotherOSDownload")}</a></p>
+                    }
             </div>
 
         </footer>
--- src/pages/Player/Header.jsx
+++ src/pages/Player/Header.jsx
@@ -15,6 +15,17 @@ const Header = (props) => {
     const windowsLink = 'http://easylang.app/downloads/EasyLang.Player-Beta_win_prod.exe';
     // http://easylang.app/downloads/EasyLang.Player-Beta_mac_prod.dmg
 
+    let OSName="Unknown OS";
+
+    function findOSName(){
+       
+        if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+        if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+        // if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+        // if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+    }
+    findOSName()
+
     return (
         <header className="getStartedPlayer-header">
             <div className="wrapper-getStarted">
@@ -24,22 +35,32 @@ const Header = (props) => {
                     <h1 className="getStarted-title">{getLangText("GetStartedPlayerText")}</h1>
                 </div>
                 <div className="getStartedPlayer-button">
-                    <a
-                        className="startedPlayer-button top"
-                        href={macLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <span>{getLangText("GetPlayerButtonMac")}</span>
-                    </a>
-                    <a
-                        className="startedPlayer-button top"
-                        href={windowsLink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <span>{getLangText("GetPlayerButtonWindows")}</span>
-                    </a>
+                    { OSName === "Windows" &&
+                        <a
+                            className="startedPlayer-button top"
+                            href={windowsLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <span>{getLangText("GetPlayerButtonWindows")}</span>
+                        </a>
+                    }
+                    { OSName === "Windows" &&
+                        <p>{getLangText("IfButtonIsForWindows")} <a href={macLink}>{getLangText("LinkToAnotherOSDownload")}</a></p>
+                    }
+                    { OSName === "MacOS" &&
+                        <a
+                            className="startedPlayer-button top"
+                            href={windowsLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                            <span>{getLangText("GetPlayerButtonMac")}</span>
+                        </a>
+                    }
+                    { OSName === "MacOS" &&
+                        <p>{getLangText("IfButtonIsForMac")} <a href={windowsLink}>{getLangText("LinkToAnotherOSDownload")}</a></p>
+                    }
                 </div>
                 <div>
                     <p className="getStarted-subtitle">{getLangText("GetStartedBottomText")}</p>
