ae74c4b8a48ff9b99bdf3f66249208f8000cfb24
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-06-11 11:19:47 +0300

    A good solution for footer-nav-bar

--- src/components/FooterNavBar/index.jsx
+++ src/components/FooterNavBar/index.jsx
@@ -3,14 +3,20 @@ import "./footerNavBar.scss";
 import { Link } from "@reach/router";
 import ReactHtmlParser from "react-html-parser";
 
-const generalRenderingConditionProd = "https://easylang.app/";
-const extensionRenderingConditionProd = "https://easylang.app/extension";
-const playerRenderingConditionProd = "https://easylang.app/player";
+const generalRenderingConditionProd = ["http://easylang.app/", "http://easylang.app/en", "http://easylang.app/es",
+"http://easylang.app/zh", "http://easylang.app/ru"];
+const extensionRenderingConditionProd = ["http://easylang.app/extension", "http://easylang.app/en/extension", "http://easylang.app/es/extension",
+"http://easylang.app/zh/extension", "http://easylang.app/ru/extension"];
+const playerRenderingConditionProd = ["http://easylang.app/player", "http://easylang.app/en/player", "http://easylang.app/es/player",
+"http://easylang.app/zh/player", "http://easylang.app/ru/player"];
 
 
-const generalRenderingConditionDev = "https://easylang.app/";
-const extensionRenderingConditionDev = "https://easylang.app/extension";
-const playerRenderingConditionDev = "https://easylang.app/player";
+const generalRenderingConditionDev = ["http://localhost:3000/", "http://localhost:3000/en", "http://localhost:3000/es",
+"http://localhost:3000/zh", "http://localhost:3000/ru"];
+const extensionRenderingConditionDev = ["http://localhost:3000/extension", "http://localhost:3000/en/extension", "http://localhost:3000/es/extension",
+"http://localhost:3000/zh/extension", "http://localhost:3000/ru/extension"];
+const playerRenderingConditionDev = ["http://localhost:3000/player", "http://localhost:3000/en/player", "http://localhost:3000/es/player",
+"http://localhost:3000/zh/player", "http://localhost:3000/ru/player"];
 
 export default class FooterNavBar extends Component{
 
@@ -19,9 +25,10 @@ export default class FooterNavBar extends Component{
     };
 
     render(){
+
         return(
             <div id="footer-nav-bar">
-                { window.location.href === generalRenderingConditionProd &&
+                { generalRenderingConditionProd.find(e => e === window.location.href) &&
                 <div className="footer-nav-bar_links" id="landing-footer-nav-bar">
 
                     <Link to="/">
@@ -38,7 +45,7 @@ export default class FooterNavBar extends Component{
 
                 </div> }
 
-                { window.location.href === extensionRenderingConditionProd &&
+                { extensionRenderingConditionProd.find(e => e === window.location.href) &&
                 <div className="footer-nav-bar_links" id="extension-footer-nav-bar">
 
                     <Link to="/">
@@ -55,7 +62,7 @@ export default class FooterNavBar extends Component{
 
                 </div> }
 
-                { window.location.href === playerRenderingConditionProd &&
+                { playerRenderingConditionProd.find(e => e === window.location.href) &&
                 <div className="footer-nav-bar_links" id="player-footer-nav-bar">
                     
                     <Link to="/">
