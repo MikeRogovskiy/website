ab3aff35778891ea18a08dd45cb2bb3d7e28996f
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-06-11 16:22:25 +0300

    Some new test script for ChatBot

--- src/components/ChatBotSimple/index.jsx
+++ src/components/ChatBotSimple/index.jsx
@@ -23,17 +23,27 @@ const steps = [
         id: '1',
         message: 'What is your name?',
         trigger: '2',
-      },
-      {
+    },
+    {
         id: '2',
         user: true,
         trigger: '3',
-      },
-      {
+    },
+    {
         id: '3',
         message: 'Hi {previousValue}, nice to meet you!',
+        trigger: '4',
+    },
+    {
+        id: '4',
+        user: true,
+        trigger: '5',
+    },
+    {
+        id: '5',
+        message: 'I am not programed well to understand {previousValue}',
         end: true,
-      },
+    },
 ]
 
 
