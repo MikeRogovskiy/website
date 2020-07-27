acef6ae12e9633ffea9b2e53262329dabd1dd85b
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-07-27 15:28:32 +0300

    Small bug fix

---
+++ src/components/MobilePopUp/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function MobilePopUp(props){
+    return(
+        <div id="mobile-pop-up">
+                <div className="pop-up_conteiner">
+                    <div className="pop-up_conteiner_content">
+                        <div className="pop-up_conteiner_content_header">
+
+                        </div>
+                        <div className="pop-up_conteiner_content_main">
+                            
+                        </div>
+                        <div className="pop-up_conteiner_content_footer">
+                            
+                        </div>
+                    </div>
+                </div>
+        </div>
+    )
+}
