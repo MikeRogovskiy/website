33e2711a5ad21420deb35ca089e06b74c75c6585
Aliaksei Kernoha <halflife.kernoga@yandex.by>
2020-08-06 15:22:38 +0300

    Small fixes.

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -17,7 +17,7 @@ export default function ContactUs(props){
         <div className="contact-us">
             <div className="contact-us_container">
                 <div className="contact-us_container_text">
-                    <div classNmae="contact-us_container_text_block">
+                    <div className="contact-us_container_text_block">
                         <h2>{getLangText("title1")}</h2>
                         <p>{getLangText("theName")}</p>
                         <p>{getLangText("index")}</p>
@@ -46,10 +46,10 @@ export default function ContactUs(props){
                 </div>
                 <div className="contact-us_container_map">
                     <iframe
-                        frameborder="0"
+                        frameBorder="0"
                         scrolling="yes"
-                        marginheight="0"
-                        marginwidth="0"
+                        marginHeight="0"
+                        marginWidth="0"
                         id="gmap_canvas"
                         src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=vulica%20Talbuchina%202,%20Minsk%20Minsk+()&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
                     </iframe>
