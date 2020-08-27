393c49878cc578b0b117552228cc723281789482
6alex6kash6 <6alex6kash6@gmail.com>
2020-08-27 16:01:03 +0300

    fix: code refactoring

--- src/components/FooterNavBar/index.jsx
+++ src/components/FooterNavBar/index.jsx
@@ -19,96 +19,118 @@ export default class FooterNavBar extends Component {
     linkVK: "https://vk.com/public196837780",
   };
 
+  classes = {
+    footerNavBar: classNames(
+      {
+        "footer-nav-bar-b": this.props.page === "landingB",
+      },
+      {
+        "footer-nav-bar": this.props.page !== "landingB",
+      }
+    ),
+  };
+
   footerNavBarContent = (
-    <div id="footer-nav-bar_content">
-      <Link
-        to="/contact"
-        onClick={() => {
-          window.gtag("event", "Contact Us click", {
-            event_category: "Landing. Footer",
-          });
-        }}
-      >
-        <button>{this.getLangText("ContactUs")}</button>
-      </Link>
+    <div
+      className="footer-nav-bar_links"
+      id={
+        this.props.page === "landing"
+          ? "landing-footer-nav-bar"
+          : "extension-footer-nav-bar"
+      }
+    >
+      <div id="footer-nav-bar_content">
+        <Link
+          to="/contact"
+          onClick={() => {
+            window.gtag("event", "Contact Us click", {
+              event_category: "Landing. Footer",
+            });
+          }}
+        >
+          <button>{this.getLangText("ContactUs")}</button>
+        </Link>
 
-      <Link
-        to="/about"
-        onClick={() => {
-          window.gtag("event", "About Us click", {
-            event_category: "Landing. Footer",
-          });
-        }}
-      >
-        <button>{this.getLangText("AboutUs")}</button>
-      </Link>
+        <Link
+          to="/about"
+          onClick={() => {
+            window.gtag("event", "About Us click", {
+              event_category: "Landing. Footer",
+            });
+          }}
+        >
+          <button>{this.getLangText("AboutUs")}</button>
+        </Link>
 
-      <Link
-        to="/privacy"
-        onClick={() => {
-          window.gtag("event", "Privacy Policy click", {
-            event_category: "Landing. Footer",
-          });
-        }}
-      >
-        <button>{this.getLangText("PrivacyPolicy")}</button>
-      </Link>
-      <Footer />
+        <Link
+          to="/privacy"
+          onClick={() => {
+            window.gtag("event", "Privacy Policy click", {
+              event_category: "Landing. Footer",
+            });
+          }}
+        >
+          <button>{this.getLangText("PrivacyPolicy")}</button>
+        </Link>
+        <Footer />
+      </div>
     </div>
   );
   footerNavBarContentB = (
-    <div id="footer-nav-bar_content-b">
-      <div id="footer-nav-bar-text">
-        <div id="footer-nav-bar-text-links">
-          <Link
-            to="/contact"
-            onClick={() => {
-              window.gtag("event", "Contact Us click", {
-                event_category: "Landing B. Footer",
-              });
-            }}
-          >
-            <button>{this.getLangText("ContactUs")}</button>
-          </Link>
+    <div className="footer-nav-bar_links" id="landing-b-footer-nav-bar">
+      <div id="footer-nav-bar_content-b">
+        <div id="footer-nav-bar-text">
+          <div id="footer-nav-bar-text-links">
+            <Link
+              to="/contact"
+              onClick={() => {
+                window.gtag("event", "Contact Us click", {
+                  event_category: "Landing B. Footer",
+                });
+              }}
+            >
+              <button>{this.getLangText("ContactUs")}</button>
+            </Link>
 
-          <Link
-            to="/about"
-            onClick={() => {
-              window.gtag("event", "About Us click", {
-                event_category: "Landing B. Footer",
-              });
-            }}
-          >
-            <button>{this.getLangText("AboutUs")}</button>
-          </Link>
+            <Link
+              to="/about"
+              onClick={() => {
+                window.gtag("event", "About Us click", {
+                  event_category: "Landing B. Footer",
+                });
+              }}
+            >
+              <button>{this.getLangText("AboutUs")}</button>
+            </Link>
 
-          <Link
-            to="/privacy"
-            onClick={() => {
-              window.gtag("event", "Privacy Policy click", {
-                event_category: "Landing B. Footer",
-              });
-            }}
-          >
-            <button>{this.getLangText("PrivacyPolicy")}</button>
-          </Link>
+            <Link
+              to="/privacy"
+              onClick={() => {
+                window.gtag("event", "Privacy Policy click", {
+                  event_category: "Landing B. Footer",
+                });
+              }}
+            >
+              <button>{this.getLangText("PrivacyPolicy")}</button>
+            </Link>
+          </div>
+          <p>© 2020 EASYLANG CO. All Rights Reserved.</p>
         </div>
-        <p>© 2020 EASYLANG CO. All Rights Reserved.</p>
-      </div>
-      <div id="footer-nav-bar-logos">
-        <a
-          href={this.links.linkInstagram}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <img src={instagram}></img>
-        </a>
-        <a href={this.links.linkVK} target="_blank" rel="noreferrer noopener">
-          <img src={vkontakte}></img>
-        </a>
-        <a href={this.links.linkFB} target="_blank" rel="noreferrer noopener">
-          <img src={facebook}></img>
-        </a>
+        <div id="footer-nav-bar-logos">
+          <a
+            href={this.links.linkInstagram}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <img src={instagram}></img>
+          </a>
+          <a href={this.links.linkVK} target="_blank" rel="noreferrer noopener">
+            <img src={vkontakte}></img>
+          </a>
+          <a href={this.links.linkFB} target="_blank" rel="noreferrer noopener">
+            <img src={facebook}></img>
+          </a>
+        </div>
       </div>
     </div>
   );
@@ -118,29 +140,18 @@ export default class FooterNavBar extends Component {
   }
 
   render() {
-    return (
-      <div
-        className={
-          this.props.page === "landingB" ? "footer-nav-bar-b" : "footer-nav-bar"
-        }
-      >
-        {this.props.page === "landingB" ? (
-          <div className="footer-nav-bar_links" id="landing-b-footer-nav-bar">
-            {this.footerNavBarContentB}
-          </div>
-        ) : (
-          <div
-            className="footer-nav-bar_links"
-            id={
-              this.props.page === "landing"
-                ? "landing-footer-nav-bar"
-                : "extension-footer-nav-bar"
-            }
-          >
-            {this.footerNavBarContent}
-          </div>
-        )}
-      </div>
-    );
+    if (this.props.page === "landingB") {
+      return (
+        <div className={this.classes.footerNavBar}>
+          {this.footerNavBarContentB}
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.classes.footerNavBar}>
+          {this.footerNavBarContent}
+        </div>
+      );
+    }
   }
 }
