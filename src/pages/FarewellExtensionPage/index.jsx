a0098cf0672fd5bde3fccf8840ef13d95e8d5af9
Alexandr <6alex6kash6@gmail.com>
2020-09-02 17:43:57 +0300

    feat: done markup for farewell page

--- src/pages/FarewellExtensionPage/index.jsx
+++ src/pages/FarewellExtensionPage/index.jsx
@@ -3,6 +3,24 @@ import "./FarewellExtensionPage.scss";
 import brainCry from "../../assets/images/brainCry.svg";
 
 export default function FarewellExtensionPage() {
+  const OPTIONS = [
+    "Hard to use",
+    "I don’t want to create account",
+    "Bad design",
+    "Doesn’t work on my favorite websites",
+    "Too expensive",
+    "Not needed",
+  ];
+
+  const createCheckbox = (option) => (
+    <label>
+      <input type="checkbox" className="checkbox" />
+      {option}
+    </label>
+  );
+
+  const createCheckboxes = OPTIONS.map((item) => createCheckbox(item));
+
   return (
     <div className="farewell-main">
       <div className="farewell-main__left">
@@ -27,6 +45,18 @@ export default function FarewellExtensionPage() {
       </div>
       <div className="farewell-main__right">
         <p className="farewell-main__right--title">Tell us why you leave?</p>
+        <form className="farewell-main__right--form">
+          {createCheckboxes}
+          <div>
+            <p>
+              Other reasons <span style={{ color: "#BDBDBD" }}>(Optional)</span>{" "}
+            </p>
+            <textarea name="" id="" cols="40" rows="7"></textarea>
+          </div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
