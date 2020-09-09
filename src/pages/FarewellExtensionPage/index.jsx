1d0163d8d68f67ab6444ff351fa19d156554a9c5
6alex6kash6 <6alex6kash6@gmail.com>
2020-09-09 13:30:12 +0300

    add more analytics events

--- src/pages/FarewellExtensionPage/index.jsx
+++ src/pages/FarewellExtensionPage/index.jsx
@@ -5,6 +5,7 @@ import thankYou from "../../assets/images/thank-you.svg";
 
 export default function FarewellExtensionPage() {
   const [showModal, setShowModal] = useState(false);
+  const [textareaValue, setTextareaValue] = useState("");
 
   const OPTIONS = [
     "Hard to use",
@@ -17,7 +18,15 @@ export default function FarewellExtensionPage() {
 
   const createCheckbox = (option, key) => (
     <label key={key}>
-      <input type="checkbox" className="checkbox" />
+      <input
+        type="checkbox"
+        className="checkbox"
+        onChange={() => {
+          window.gtag("event", option, {
+            event_category: "Reinstall survey. Checkbox",
+          });
+        }}
+      />
       {option}
     </label>
   );
@@ -27,6 +36,9 @@ export default function FarewellExtensionPage() {
   const handleSubmit = (submitEvent) => {
     submitEvent.preventDefault();
     setShowModal(true);
+    window.gtag("event", textareaValue, {
+      event_category: "Reinstall survey. Submit",
+    });
   };
 
   const modalWindow = (
@@ -72,7 +84,14 @@ export default function FarewellExtensionPage() {
             <p>
               Other reasons <span style={{ color: "#BDBDBD" }}>(Optional)</span>{" "}
             </p>
-            <textarea name="" id="" cols="40" rows="7"></textarea>
+            <textarea
+              name=""
+              id=""
+              cols="40"
+              rows="7"
+              value={textareaValue}
+              onChange={(event) => setTextareaValue(event.target.value)}
+            ></textarea>
           </div>
           <button type="submit" className="submit-button">
             Submit
